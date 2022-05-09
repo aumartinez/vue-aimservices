697eefbda57ddd02f76736ac4ee5d1ab4d9600aa
Augusto Martinez <acm_asd@hotmail.com>
2022-05-09 16:35:28 -0600

    Update SiteHeader.vue

--- src/views/SiteHeader.vue
+++ src/views/SiteHeader.vue
@@ -5,7 +5,7 @@
         <div class="col-12">
           <nav class="navbar navbar-expand-lg">            
             <a class="navbar-brand" href="/">
-              <img src="./imgs/logo.png" class="img-responsive" />
+              <img src="imgs/logo.png" class="img-responsive" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
